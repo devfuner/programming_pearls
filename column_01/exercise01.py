85f4ea31005a1825fc880c130dc49a141ecca49b
devfuner <devfuner@gmail.com>
2015-08-03 11:00:01 +0900

    update profile data

---
+++ column_01/exercise01.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+#   Date : 2015-08-02
+#   Name : exercise01.py
+#   Version : 0.0.1
+#   Mail : devfuner@gmail.com
+#   Twitter : http://twitter.com/devfuner
+# ==============================================================================
+
+import os
+from random import shuffle
+from memory_profiler import profile
+
+
+@profile
+def init_file(filename):
+    with open(filename, 'w') as f:
+        for num in unique_list():
+            print(num, file=f)
+
+
+@profile
+def unique_list():
+    """
+    특정 범위의 유니크한 숫자 리스트를 가져오는 함수
+    """
+    arr = []
+    for i in range(10 ** 6, 10 ** 7):
+        arr.append(i)
+    shuffle(arr)
+    return arr
+
+
+@profile
+def check_unique(_list):
+    from collections import Counter
+    cnts = Counter(_list).most_common()
+    for cnt in cnts:
+        word, c = cnt
+        if c > 1:
+            print('ALERT!!!', word)
+
+
+@profile
+def read_file():
+    nums = []
+    with open('number.txt', 'r') as f:
+        for line in f.readlines():
+            nums.append(int(line))
+    return nums
+
+
+@profile
+def sorted_file(nums):
+    with open('sorted.txt', 'w') as f:
+        for num in nums:
+            print(num, file=f)
+
+
+if __name__ == '__main__':
+    limit = 10 ** 7
+    filename = 'number.txt'
+    if not os.path.exists(filename):
+        init_file(filename, limit)
+    nums = read_file()
+    nums.sort()
+    sorted_file(nums)
---
+++ column_01/exercise01_result.md
@@ -0,0 +1,24 @@
+```
+devfunerui-MacBook-Air:column_01 devfuner$ python -m memory_profiler exercise01.py
+Filename: exercise01.py
+
+Line #    Mem usage    Increment   Line Contents
+================================================
+    46     10.2 MiB      0.0 MiB   @profile
+    47                             def read_file():
+    48     10.2 MiB      0.0 MiB       nums = []
+    49     10.2 MiB      0.0 MiB       with open('number.txt', 'r') as f:
+    50    457.8 MiB    447.6 MiB           for line in f.readlines():
+    51    457.8 MiB      0.0 MiB               nums.append(int(line))
+    52     51.2 MiB   -406.5 MiB       return nums
+
+Filename: exercise01.py
+
+Line #    Mem usage    Increment   Line Contents
+================================================
+    55    423.9 MiB      0.0 MiB   @profile
+    56                             def sorted_file(nums):
+    57    423.9 MiB      0.1 MiB       with open('sorted.txt', 'w') as f:
+    58    424.1 MiB      0.2 MiB           for num in nums:
+    59    424.1 MiB      0.0 MiB               print(num, file=f)
+```
